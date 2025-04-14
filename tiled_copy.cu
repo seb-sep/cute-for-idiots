d10d8ff6bb6c7807f164bd5ea2777f0aef54f57a
seb-sep <sebmichaelsep@gmail.com>
2025-04-14 13:29:05 -0400

    working copy kernel

--- tiled_copy.cu
+++ tiled_copy.cu
@@ -20,46 +20,48 @@ using namespace cute;
 /*
     of course you would never make a copy kernel by bouncing through smem, but just for practice
 */
-template <class ProblemShape, class CtaTiler>
-__global__ void tiled_shared_copy_kernel(ProblemShape shapeMN, CtaTiler tiler, float* A, float* B) {
+template <class ProblemShape, class CtaTiler, class ThreadTiler>
+__global__ void tiled_shared_copy_kernel(ProblemShape shapeMN, CtaTiler cta_tiler, ThreadTiler thread_tiler, float* A, float* B) {
     // instantiate global tensors
-    printf("hello, world\n");
-    Tensor a = make_tensor(make_gmem_ptr(A), make_layout(shapeMN, LayoutRight{}));
-    Tensor b = make_tensor(make_gmem_ptr(B), make_layout(shapeMN, LayoutRight{}));
+    // default layout is layoutLeft (col-major)
+    Tensor a = make_tensor(make_gmem_ptr(A), make_layout(shapeMN));
+    Tensor b = make_tensor(make_gmem_ptr(B), make_layout(shapeMN));
 
-    __shared__ float copy_S[size(tiler)]; // cosize_v statically gets the size of a layout
-    Tensor shared_tile = make_tensor(make_smem_ptr(copy_S), tiler);
+    __shared__ float smem_A[size(cta_tiler)];
+    Tensor sA = make_tensor(make_smem_ptr(smem_A), cta_tiler);
 
     // tile of A and B
     // the tiler is the shape of the actual inner tile
-    Tensor a_tiled = local_tile(a, tiler, make_coord(blockIdx.y, blockIdx.x));
-    Tensor b_tiled = local_tile(b, tiler, make_coord(blockIdx.y, blockIdx.x));
-        printf("a_tiled: ");
-        cute::print(shape(a_tiled));
-        printf("b_tiled: ");
-        cute::print(shape(b_tiled));
-    // Tensor b_tiled = zipped_divide(a, tiler)(make_coord(_, _), make_coord(blockIdx.x, blockIdx.y)); 
+    Tensor gA = local_tile(a, cta_tiler, make_coord(blockIdx.x, blockIdx.y));
+    Tensor gB = local_tile(b, cta_tiler, make_coord(blockIdx.x, blockIdx.y));
+    if (thread0()) {
+        cute::print(zipped_divide(gA, cta_tiler));
+        printf("\n");
+        cute::print(gB);
+        printf("\n");
+    }
 
-    // these formulations for tiling should be the SAME
-    // assert(layout<0>(a_tiled) == layout<0>(b_tiled));
-    // assert(layout<1>(a_tiled) == layout<1>(b_tiled));
-    // assert(stride<0>(a_tiled) == stride<0>(b_tiled));
-    // assert(stride<1>(a_tiled) == stride<1>(b_tiled));
 
-    // CUTE_STATIC_ASSERT_V((shape<0>(a_tiled)) == (shape<0>(b_tiled)));
-    // CUTE_STATIC_ASSERT_V((shape<1>(a_tiled)) == (shape<1>(b_tiled)));
-    // CUTE_STATIC_ASSERT_V((stride<0>(a_tiled)) == (stride<0>(b_tiled)));
-    // CUTE_STATIC_ASSERT_V((stride<1>(a_tiled)) == (stride<1>(b_tiled)));
+    // we defined a 1d threadblock with the size of the thread tiler, so we _only_
+    // need to use threadIdx.x here
+    // local_partition selects over the outer mode, NOT the inner tile mode like 
+    // local_tile does. This way, each threadIdx.x is not given a contiuous tile, instead,
+    // each tile is evenly split across all threads, instea of one tile per thread
+    Tensor tgA = local_partition(gA, thread_tiler, threadIdx.x);
+    Tensor tsA = local_partition(sA, thread_tiler, threadIdx.x);
+    Tensor tgB = local_partition(gB, thread_tiler, threadIdx.x);
 
-    // // inner mode of tile should have same layout as smem tile
-    // CUTE_STATIC_ASSERT_V((shape<0>(a_tiled)) == (shape<0>(shared_tile)));
-    // CUTE_STATIC_ASSERT_V((shape<1>(a_tiled)) == (shape<1>(shared_tile)));
-    // CUTE_STATIC_ASSERT_V((stride<0>(a_tiled)) == (stride<0>(shared_tile)));
-    // CUTE_STATIC_ASSERT_V((stride<1>(a_tiled)) == (stride<1>(shared_tile)));
-
-    copy(a_tiled, shared_tile);
+    if (thread0()) {
+        cute::print(zipped_divide(tgA, thread_tiler));
+        printf("\n");
+        cute::print(tsA);
+        printf("\n");
+        cute::print(tgB);
+        printf("\n");
+    }
+    copy(tgA, tsA);
     __syncthreads();
-    copy(shared_tile, b_tiled);
+    copy(tsA, tgB);
 }
 
 int main(int argc, char** argv) {
@@ -69,7 +71,8 @@ int main(int argc, char** argv) {
   //
 
   // Define a tensor shape with dynamic extents (m, n)
-  auto tensor_shape = make_shape(256, 512);
+  auto tensor_shape = make_shape(128, 64);
+  // auto tensor_shape = make_shape(256, 512);
 
   //
   // Allocate and initialize
@@ -78,12 +81,47 @@ int main(int argc, char** argv) {
   thrust::host_vector<float> h_S(size(tensor_shape));
   thrust::host_vector<float> h_D(size(tensor_shape));
 
-  for (size_t i = 0; i < h_S.size(); ++i) {
+  auto cta_tiler = make_shape(Int<128>{}, Int<64>{});
+  auto thread_tiler = make_layout(make_shape(Int<32>{}, Int<4>{}));
+
+  // Tensor tensor_hS = make_tensor(h_S.data(), make_layout(tensor_shape));
+  // Tensor tensor_hD = make_tensor(h_D.data(), make_layout(tensor_shape));
+  // printf("tensor_hS layout: ");
+  // cute::print(layout(tensor_hS));
+  // printf("\n");
+  // printf("thread_tiler layout: ");
+  // cute::print(layout(thread_tiler));
+  // printf("\n");
+
+for (size_t i = 0; i < h_S.size(); ++i) {
     h_S[i] = i;
     h_D[i] = 0.0;
   }
 
-    // does this copy to device??? sweet
+
+  // // Then partition within the block
+  // for (size_t i = 0; i < size(thread_tiler); ++i) {
+  //   auto tile_s = local_partition(tensor_hS, thread_tiler, i);
+  //   auto tile_d = local_partition(tensor_hD, thread_tiler, i);
+    
+  //   if (i < 2) {  // Print first couple iterations
+  //       printf("Thread %zu:\n", i);
+  //       printf("Source tile: ");
+  //       cute::print(tile_s);
+  //       printf("\nDestination tile: ");
+  //       cute::print(tile_d);
+
+  //       printf("\nFirst few elements: %f %f %f %f\n", 
+  //              float(tile_s(0)), float(tile_s(1)), 
+  //              float(tile_s(2)), float(tile_s(3)));
+  //   }
+    
+  //   copy(tile_s, tile_d);
+  // }
+
+
+
+  // does this copy to device??? sweet
   thrust::device_vector<float> d_S = h_S;
   thrust::device_vector<float> d_D = h_D;
 
@@ -100,14 +138,13 @@ int main(int argc, char** argv) {
 
   // Define a statically sized block (M, N).
   // Note, by convention, capital letters are used to represent static modes.
-  auto block_shape = make_shape(Int<128>{}, Int<64>{});
 
-  if ((size<0>(tensor_shape) % size<0>(block_shape)) || (size<1>(tensor_shape) % size<1>(block_shape))) {
+  if ((size<0>(tensor_shape) % size<0>(cta_tiler)) || (size<1>(tensor_shape) % size<1>(cta_tiler))) {
     std::cerr << "The tensor shape must be divisible by the block shape." << std::endl;
     return -1;
   }
   // Equivalent check to the above
-  if (not evenly_divides(tensor_shape, block_shape)) {
+  if (not evenly_divides(tensor_shape, cta_tiler)) {
     std::cerr << "Expected the block_shape to evenly divide the tensor shape." << std::endl;
     return -1;
   }
@@ -118,19 +155,21 @@ int main(int argc, char** argv) {
   // These will be used to determine the CUDA kernel grid dimensions.
   // tiled_divide has the tile mode first like zipped_divide, but the outer modes are flattened out unlike
   // in zipped_divide where they're collected into a single tuple mode
-  Tensor tiled_tensor_S = tiled_divide(tensor_S, block_shape);      // ((M, N), m', n')
-  Tensor tiled_tensor_D = tiled_divide(tensor_D, block_shape);      // ((M, N), m', n')
+  Tensor tiled_tensor_S = tiled_divide(tensor_S, cta_tiler);      // ((M, N), m', n')
+  Tensor tiled_tensor_D = tiled_divide(tensor_D, cta_tiler);      // ((M, N), m', n')
 
     // having done a tiled divide lets us easily extract the modes instead of doing nested access crap
-    cute::print(shape(tiled_tensor_D));
-  dim3 gridDim (2, 8);   // Grid shape corresponds to modes m' and n'
-//   dim3 gridDim (size<1>(tiled_tensor_D), size<2>(tiled_tensor_D));   // Grid shape corresponds to modes m' and n'
-  dim3 blockDim(128);
+   cute::print(shape(tiled_tensor_D));
+    printf("gridDim: %d, %d\n", size<1>(tiled_tensor_D), size<2>(tiled_tensor_D));
+  fflush(stdout);
+  // dim3 gridDim (2, 8);   // Grid shape corresponds to modes m' and n'
+  dim3 gridDim (size<1>(tiled_tensor_D), size<2>(tiled_tensor_D));   // Grid shape corresponds to modes m' and n'
+  dim3 blockDim(size(thread_tiler));
 
   //
   // Launch the kernel
   //
-tiled_shared_copy_kernel<<<gridDim, blockDim>>>(tensor_shape, block_shape, thrust::raw_pointer_cast(d_S.data()), thrust::raw_pointer_cast(d_D.data()));
+tiled_shared_copy_kernel<<<gridDim, blockDim>>>(tensor_shape, cta_tiler, thread_tiler, thrust::raw_pointer_cast(d_S.data()), thrust::raw_pointer_cast(d_D.data()));
 fflush(stdout);
   CUDA_CHECK(cudaDeviceSynchronize());
     CUDA_CHECK(cudaGetLastError());
@@ -139,6 +178,7 @@ fflush(stdout);
   //   std::cerr << "CUDA Runtime error: " << cudaGetErrorString(result) << std::endl;
   //   return -1;
   // }
+
 
   //
   // Verify
